e2566f5007e72549e1b53610143e8d382d33f717
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-01 17:40:54 +0200

    Create create_account.py

---
+++ create_account.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Import the Python module.
+from tronapi import Tron
+
+# Instantiate Tron.
+tron = Tron()
+
+# Create an account object.
+account = tron.create_account
+
+# Create a private key.
+private_key = account.private_key
+
+# Create a public key.
+public_key = account.public_key
+
+# Create a Tron address.
+tron_address = account.address.base58
+
+# Print result to screen.
+print("Public Key: ", public_key)
+print("Private Key: ", private_key)
+print("Tron Address: ", tron_address)
