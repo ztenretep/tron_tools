8584e84af0fe00235fca64c231ad12afdbe363cc
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-02 19:55:23 +0200

    Update tron_vote_balance.py

--- tron_vote_balance.py
+++ tron_vote_balance.py
@@ -10,36 +10,36 @@ Super Representatives can be found at https://tronscan.org/. Signed
 in it can be checked if staking as well as voting is working.
 """
 
-# Import the Python module.
+# Import the third party Python module.
 from tronapi import Tron
 
-# Set public key.
+# Set the public key.
 public_key = '<public_key>'
 
-# Set private key.
+# Set the private key.
 private_key = '<private_key>'
 
 # Set the Super Representative.
-SR = "<superrepresentive>"
+SR = "<superrepresentative>"
 
-# Instantiate Tron.
+# Create a new instance of class Tron().
 tron = Tron()
 
-# Assign private key to Tron().
+# Assign private key to an attribute of new instance tron.
 tron.private_key = private_key
 
-# Assign public key to Tron().
+# Assign public key to an attribute of new instance tron.
 tron.default_address = source
 
-# Set number of votes. type int.
+# Set the number of votes. Number of votes must be of type int.
 nov = '<number_of_votes>'
 
-# Vote on number of votes.
+# Vote with the number of votes on the Super.
 vote = tron.transaction_builder.vote([(SR, nov)], public_key)
 
 # In tronapi a method for sign and broadcast for vote is missing.
 vote = tron.trx.sign(vote)
 vote = tron.trx.broadcast(vote)
 
-# Print response.
+# Print response of voting.
 print(vote)
