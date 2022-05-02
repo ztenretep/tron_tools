f17a5b9ad7d610c7056b3682dff9dd0cf5ef8cd8
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-02 14:46:23 +0200

    Create tron_claim_transaction.py

---
+++ tron_claim_transaction.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Claim Tron block rewards.
+
+Private key as well as public key of sender are defined via class attributes. Receiver 
+and amount are method arguments.
+"""
+
+# Import the Python module.
+from tronapi import Tron
+
+# Set address (public key) to send Tron from.
+source = '<public_key>'
+
+# Set private key of address to send Tron from.
+private_key = '<private_key>'
+
+# Instantiate Tron.
+tron = Tron()
+
+# Assign private key to Tron() here.
+tron.private_key = private_key
+
+# Assign public key to Tron() here.
+tron.default_address = source
+
+withdraw = tron.transaction_builder.withdraw_block_rewards(source)
+
+# In trx is no method for withdraw_block_rewards.
+withdraw = tron.trx.sign(withdraw)
+withdraw = tron.trx.broadcast(withdraw)
+
+# Print response.
+print(withdraw)
