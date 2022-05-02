24c5baf527975dd1aca37f3f6b3150acf97c47dd
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-02 08:32:55 +0200

    Update tron_freeze_in_work.py

--- tron_freeze_in_work.py
+++ tron_freeze_in_work.py
@@ -7,8 +7,6 @@ Function freeze_balance of class trx (Trx -> mapped to trx) is calling function
 freeze_balance of class transaction_builder (TransactionBuilder -> mapped to 
 transaction_builder).
 
-Signing and broadcasting is done from within send_transaction (TransactionBuilder).
-
 Private key as well as public key of sender are defined via class attributes. Receiver 
 and amount are method arguments.
 
@@ -27,14 +25,17 @@ source = '<source_address>'
 # Set private key of address to send Tron from.
 private_key = '<private_key>'
 
-# Set TRX amount to freeze.
-amount = 1.0
+# Set TRX amount to freeze. Amount is of type int.
+amount = 1
 
+# Set duration for freezing. duration is of type int.
 duration = 3
 
-resource = 'BANDWIDTH'
+# Set what to freeze.
+#resource = 'BANDWIDTH'
 resource = 'ENERGY'
 
+# Set account for the moment to None.
 account = None
 
 # Instantiate Tron.
@@ -46,6 +47,7 @@ tron.private_key = private_key
 # Assign public key to Tron() here.
 tron.default_address = source
 
+# Freeze the amount of money.
 freeze = tron.trx.freeze_balance(amount, duration, 'BANDWIDTH', account)
 
 # Print response.
