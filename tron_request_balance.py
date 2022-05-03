bc0addcb8d0221e1bce2776a23ec6c806d58bc23
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 13:43:23 +0200

    Update tron_request_balance.py

--- tron_request_balance.py
+++ tron_request_balance.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""Request the Tron balance of a given Tron address from the blockchain.
+"""
+# pylint: disable=invalid-name
+# pylint: disable=no-member
 
-# Import the Python modules.
+# Import the third party Python module.
 from tronapi import Tron
 
 # Set the Tron address.
-TRON_ADDRESS="TLrj4MzaitvA2jRH7a8G4f8JtqhTsTg4sy"
+TRON_ADDR = "TLrj4MzaitvA2jRH7a8G4f8JtqhTsTg4sy"
 
-# Instantiate Tron.
+# Create an instance of class Tron.
 tron = Tron()
 
-# Get the balance from the network.
-balance = tron.trx.get_balance(TRON_ADDRESS)
+# Get the balance in sun from the mainnet.
+balance_sun = tron.trx.get_balance(TRON_ADDR)
 
-# Print the balance to the screen.
-print(int(balance)/1000000)
+# Convert the balance from sun to decimal.
+balance_trx = tron.fromSun(balance_sun)
+
+# Print the balance in decimal to the screen.
+print(balance_trx)
 
