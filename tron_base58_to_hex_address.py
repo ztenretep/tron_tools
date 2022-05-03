564766466537a42c29868e4d400c49699ab8744e
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 15:53:43 +0200

    Update tron_base58_to_hex_address.py

--- tron_base58_to_hex_address.py
+++ tron_base58_to_hex_address.py
@@ -9,13 +9,6 @@ tron=Tron()
 # Set Base58 address
 base58_addr = 'TLrj4MzaitvA2jRH7a8G4f8JtqhTsTg4sy'
 
-# Set hex address.
-hex_addr = '41776F8FEAD2E1E8C256A85957B37A91BD90AB5EFE'
-
 # Base 58 to hex.
 addr_hex = tron.address.to_hex(base58_addr)
 print(addr_hex)
-
-# Hex to Base58
-addr_base58 = tron.address.from_hex(hex_addr).decode("utf-8")
-print(addr_base58)
