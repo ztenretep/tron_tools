e2033445eb90ffe25b5fa4baa81e04473dac2cf6
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 15:38:13 +0200

    Create tron_account_resource.py

---
+++ tron_account_resource.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Get account resource.
+"""
+
+# Import the standard Python module.
+import json
+
+# Import Python modules
+from tronapi import Tron
+
+# Instantiate Tron.
+tron = Tron()
+
+# Set private key.
+private_key = '<private_key>'
+
+# Set public key.
+public_key = '<public_key>'
+
+# Assign private key to Tron() here.
+tron.private_key = private_key
+
+# Assign public key to Tron() here.
+tron.default_address = public_key
+
+# Get account info.
+account_resource = tron.trx.get_account_resource()
+
+# Create JSON string.
+account_resource = json.dumps(account_resource, indent=4)
+
+# Print result to screen.
+print(account_resource)
