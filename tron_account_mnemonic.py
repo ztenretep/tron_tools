99592adebe64ef8bd80e1fcb5444ed620800b6fc
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 21:04:08 +0200

    Update and rename tron_accoun_mnemonic.py to tron_account_mnemonic.py

--- tron_account_mnemonic.py
+++ tron_account_mnemonic.py
@@ -3,24 +3,24 @@
 """
 # pylint: disable=invalid-name
 
-# Import the third party Python module.
+# Import the third party Python modules.
 from bip_utils import Bip39WordsNum, Bip39MnemonicGenerator, Bip39SeedGenerator
 from bip_utils import Bip44Coins, Bip44
 
-# Generate a random mnemonic string of 12 words with default language (English).
+# Generate a random mnemonic string of 12 words from default language (English).
 mnemonic = Bip39MnemonicGenerator().FromWordsNumber(Bip39WordsNum.WORDS_NUM_12)
 print(mnemonic)
 
-# Generate seed bytes with automatic language detection and passphrase (empty).
+# Generate seed bytes with automatic language detection and empty passphrase.
 seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
 
-# Create bip44 object.
+# Create bip44 object from seed for Tron.
 bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.TRON)
 
-# Create private key.
+# Create a private key.
 b64_hex = bip44_mst_ctx.PrivateKey().Raw().ToHex()
 print(b64_hex)
 
-# Create Tron address.
+# Create a Tron address.
 addr = bip44_mst_ctx.PublicKey().ToAddress()
 print(addr)
