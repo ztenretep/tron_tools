e945ddfee7d6476bbb95231a52a1863ff7817acd
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 14:02:34 +0200

    Update tron_request_balance.py

--- tron_request_balance.py
+++ tron_request_balance.py
@@ -2,6 +2,9 @@
 """Request the Tron balance of a given Tron address from the blockchain.
 
 Balance is returned from the Mainnet in SUN. 1000000 SUN is equal to 1 TRX.
+
+The Tron address can be given as Tron address in Base 58 encoding with the
+leading T commonly used in wallets or in hex representation respectively.
 """
 # pylint: disable=invalid-name
 # pylint: disable=no-member
@@ -9,7 +12,7 @@ Balance is returned from the Mainnet in SUN. 1000000 SUN is equal to 1 TRX.
 # Import the third party Python module.
 from tronapi import Tron
 
-# Set the Tron address.
+# Set the Tron address.Base58
 TRON_ADDR = "TLrj4MzaitvA2jRH7a8G4f8JtqhTsTg4sy"
 
 # Create an instance of class Tron.
