284b049ed8ca365e64ab22b3aca835d7153136c4
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-03 15:07:25 +0200

    Update tron_vote_balance.py

--- tron_vote_balance.py
+++ tron_vote_balance.py
@@ -4,11 +4,17 @@
 If e.g. 100 Tron are frozen, then the corresponding votes are 100.
 Number of votes is in this case also 100.
 
+One has to count the number of votes. If one stake another 50 Tron,
+the new number of votes for the SR increases to 150.
+
 Private key as well as public key are defined via class attributes.
 
 Super Representatives can be found at https://tronscan.org/. Signed
 in it can be checked if staking as well as voting is working.
 """
+
+# Import the standard Python module.
+import json
 
 # Import the third party Python module.
 from tronapi import Tron
@@ -22,6 +28,9 @@ private_key = '<private_key>'
 # Set the Super Representative.
 SR = '<super_representative>'
 
+# Set the number of votes. Number of votes must be of type int.
+nov = '<number_of_votes>'
+
 # Create a new instance of class Tron().
 tron = Tron()
 
@@ -29,17 +38,16 @@ tron = Tron()
 tron.private_key = private_key
 
 # Assign public key to an attribute of new instance tron.
-tron.default_address = source
-
-# Set the number of votes. Number of votes must be of type int.
-nov = '<number_of_votes>'
+tron.default_address = public_key
 
 # Vote with the number of votes on the Super.
-vote = tron.transaction_builder.vote([(SR, nov)], public_key)
+vote_tx = tron.transaction_builder.vote([(SR, nov)], public_key)
 
 # In tronapi a method for sign and broadcast for vote is missing.
-vote = tron.trx.sign(vote)
-vote = tron.trx.broadcast(vote)
+vote = tron.trx.sign_and_broadcast(vote_tx)
 
-# Print response of voting.
-print(vote)
+# Convert list to str.
+vote_str = json.dumps(vote, indent=4)
+
+# Print response.
+print(vote_str)
