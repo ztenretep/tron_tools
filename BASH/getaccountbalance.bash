cc7878427e9e524634526f7be1a300540b81da4d
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-06 22:38:47 +0200

    Create getaccountbalance.bash

--- BASH/getaccountbalance.bash
+++ BASH/getaccountbalance.bash
@@ -1,14 +1,19 @@
 #!/usr/bin/bash
 
+# Set API url.
 URL="https://api.trongrid.io/walletsolidity/getaccount"
 
+# Set Tron address in hex.
 ADDRESS="<tron_address_in_hex>"
 
+# Create payload.
 PAYLOAD="{ \"address\": \"${ADDRESS}\" }"
 
+# Request account data.
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -X POST --data "${PAYLOAD}" "${URL}"
 
+# Exit script without error.
 exit 0
