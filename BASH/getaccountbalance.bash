524f41c61d0bb20381bb1e4d88e42b496791bdd1
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-05-06 23:30:29 +0200

    Update getaccountbalance.bash

--- BASH/getaccountbalance.bash
+++ BASH/getaccountbalance.bash
@@ -1,15 +1,25 @@
 #!/usr/bin/bash
+# #############################################################################
+# Retrieve the account and the balance information of a Tron address from the
+# blockchain.
+#
+# The URL for a request will be assembled from:
+#        Domain：https://api.trongrid.io
+# and    Path:   /walletsolidity/getaccount
+#
+# The domain remains unchanged, while the path changes with a specific request.
+# #############################################################################
 
-# Set API url.
+# Set the url for the request.
 URL="https://api.trongrid.io/walletsolidity/getaccount"
 
-# Set Tron address in hex.
+# Set the Tron address in hex.
 ADDRESS="<tron_address_in_hex>"
 
-# Create payload.
+# Create the JSON payload for the request.
 PAYLOAD="{ \"address\": \"${ADDRESS}\" }"
 
-# Request account data.
+# The balance is part of the retrieved account data.
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
