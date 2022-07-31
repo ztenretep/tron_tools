32b382531fce63bcd2fab7380124a2eb2d51f225
ztenretep <103412430+ztenretep@users.noreply.github.com>
2022-07-31 15:37:54 +0200

    Create getNextMaintenanceTime.py

---
+++ Python/TronGrid/getNextMaintenanceTime.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''Get transaction list from pending.
+
+Get transaction list information from pending pool.
+
+Reference:
+https://developers.tron.network/reference/gettransactionlistfrompending
+'''
+
+# Import the Python module.
+import json
+import requests
+
+# Set the service url for the request.
+URL = "https://api.trongrid.io/wallet/getnextmaintenancetime"
+
+# Initialise the headers.
+HEADERS = {
+    "Accept": "application/json",
+}
+
+# ====================
+# Main script function
+# ====================
+def main():
+    '''Main script function.'''
+    # Get the response from RPC server.
+    response = requests.get(URL, headers=HEADERS)
+    # Create adictionary from the response.
+    response = response.json()
+    # Create json data from dictionary.
+    response_json = json.dumps(response, indent=2)
+    # Print result to screen.
+    print(response_json)
+
+# Execute script as program or as module.
+if __name__ == "__main__":
+    # Call the main script function.
+    main()
